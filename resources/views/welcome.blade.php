303a7e9748766ab9ed09218d2812727b385ddbb6
enriquesousa <enrique.sousa@gmail.com>
2022-03-04 15:51:52 -0800

    Tailwind commit inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Tailwind</title>
+
+</head>
+<body>
+
+    <div class="container">
+       <div class="grid grid-cols-12 gap-4">
+            <div class="bg-blue-200">A</div>
+            <div class="bg-blue-300">B</div>
+            <div class="bg-blue-400">C</div>
+            <div class="bg-blue-500">D</div>
+            <div class="bg-blue-600">E</div>
+            <div class="bg-blue-700">F</div>
+            <div class="bg-blue-200">G</div>
+            <div class="bg-blue-300">H</div>
+            <div class="bg-blue-400">I</div>
+            <div class="bg-blue-500">J</div>
+            <div class="bg-blue-600">K</div>
+            <div class="bg-blue-700">L</div>
+       </div>
+    </div>
+    
+</body>
+</html>
