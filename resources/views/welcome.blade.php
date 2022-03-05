a200433a5a12f24be6c16c558ea7800353d6fe2f
enriquesousa <enrique.sousa@gmail.com>
2022-03-04 16:36:03 -0800

    Como funciona grid en Tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,21 +11,45 @@
 </head>
 <body>
 
-    <div class="container">
-       <div class="grid grid-cols-12 gap-4">
+    {{-- Diseno 1 por bloques --}}
+    {{-- <div class="container">
+       <div class="grid grid-cols-4 grid-rows-2 gap-4">
             <div class="bg-blue-200">A</div>
             <div class="bg-blue-300">B</div>
-            <div class="bg-blue-400">C</div>
+            <div class="bg-blue-400 col-span-2 row-span-2">C</div>
             <div class="bg-blue-500">D</div>
             <div class="bg-blue-600">E</div>
-            <div class="bg-blue-700">F</div>
-            <div class="bg-blue-200">G</div>
-            <div class="bg-blue-300">H</div>
-            <div class="bg-blue-400">I</div>
-            <div class="bg-blue-500">J</div>
-            <div class="bg-blue-600">K</div>
-            <div class="bg-blue-700">L</div>
        </div>
+    </div> --}}
+
+    {{-- Diseno 2 por bloques --}}
+    {{-- <div class="container">
+        <div class="grid grid-flow-col grid-rows-3">
+            <div class="bg-blue-100">1</div>
+            <div class="bg-blue-200">2</div>
+            <div class="bg-blue-300">3</div>
+            <div class="bg-blue-400">4</div>
+            <div class="bg-blue-500">5</div>
+            <div class="bg-blue-600">6</div>
+            <div class="bg-blue-700">7</div>
+            <div class="bg-blue-800">8</div>
+            <div class="bg-blue-900">9</div>
+        </div>
+    </div> --}}
+
+    {{-- Diseno 3 por bloques --}}
+    <div class="container">
+        <div class="grid grid-flow-col grid-rows-3 grid-cols-4">
+            <div class="bg-blue-100">1</div>
+            <div class="bg-blue-200">2</div>
+            <div class="bg-blue-300">3</div>
+            <div class="bg-blue-400">4</div>
+            <div class="bg-blue-500">5</div>
+            <div class="bg-blue-600">6</div>
+            <div class="bg-blue-700">7</div>
+            <div class="bg-blue-800">8</div>
+            <div class="bg-blue-900">9</div>
+        </div>
     </div>
     
 </body>
